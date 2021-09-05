d2728bca7e425755d9ab6f422957545f2a21e9d5
vn.py <xiaoyou.chen@foxmail.com>
2021-09-05 17:41:53 +0800

    [Mod] 修复typo

--- vnpy_sqlite/__init__.py
+++ vnpy_sqlite/__init__.py
@@ -27,6 +27,6 @@ from .sqlite_database import SqliteDatabase as Database
 
 
 try:
-    __version__ = importlib_metadata.version("vnpy_rqdata")
+    __version__ = importlib_metadata.version("vnpy_sqlite")
 except importlib_metadata.PackageNotFoundError:
     __version__ = "dev"
